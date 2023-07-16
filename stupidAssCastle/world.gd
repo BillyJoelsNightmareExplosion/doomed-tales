46505edb23c4525fef88d0612c0474ea4ecebfaf
Dion Tryban <github@diontryban.com>
2023-07-15 21:43:20 -0400

    Disable fullscreen

--- stupidAssCastle/world.gd
+++ stupidAssCastle/world.gd
@@ -6,7 +6,7 @@ func get_player() -> CharacterBody3D:
 # Called when the node enters the scene tree for the first time.
 func _ready():
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-    DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+#    DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
