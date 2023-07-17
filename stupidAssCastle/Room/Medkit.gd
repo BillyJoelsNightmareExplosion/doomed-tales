ab78ffe96d2c44c530bda5b4997f1b6a8318a391
Dion Tryban <github@diontryban.com>
2023-07-17 19:53:41 -0400

    Add sound to medkit

--- stupidAssCastle/Room/Medkit.gd
+++ stupidAssCastle/Room/Medkit.gd
@@ -2,17 +2,8 @@ extends Node3D
 
 @onready var player: CharacterBody3D = get_tree().root.get_children()[0].get_player()
 
-@onready var healthtext = $UserInterfaceControl/HealthValue
-@onready var ammotext = $UserInterfaceControl/AmmoValue
+@onready var audio_player: AudioStreamPlayer = get_tree().root.get_children()[0].get_medkit_audio()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
 
 func _on_area_3d_body_entered(body):
     if player.health!=100:
@@ -20,4 +11,5 @@ func _on_area_3d_body_entered(body):
             player.health=100
         else:
             player.health+=20
+        audio_player.play()
         queue_free()
--- stupidAssCastle/world.gd
+++ stupidAssCastle/world.gd
@@ -3,18 +3,11 @@ extends Node3D
 var capture_mouse = true
 
 
-
-#func _input(event):
-#    if event.is_action_pressed("ui_cancel"):
-#        capture_mouse = not capture_mouse
-#        if capture_mouse:
-#            Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#        else:
-#            Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-
 func get_player() -> CharacterBody3D:
     return $Player
     
 func get_ui():
     return $UI
+    
+func get_medkit_audio() -> AudioStreamPlayer:
+    return $MedkitAudio
