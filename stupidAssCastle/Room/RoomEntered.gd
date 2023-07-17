f3effaf7ec785047a4c3dd39dd06513e85bf7fb6
Dion Tryban <github@diontryban.com>
2023-07-16 20:19:03 -0400

    Add room trigger

---
+++ stupidAssCastle/Room/RoomEntered.gd
@@ -0,0 +1,24 @@
+extends Area3D
+
+signal start_dialogue(dialogue: Array[String])
+
+@export var dialogue: Array[String] = []
+
+@onready var room = get_parent()
+@onready var player: CharacterBody3D = get_tree().root.get_children()[0].get_player()
+
+
+func _ready():
+    $MeshInstance3D.visible = false
+    $MeshInstance3D.queue_free()
+
+
+func _on_body_entered(body):
+    room.spawn_enemies()
+    
+    # TODO: Close door behind player
+    
+    if dialogue.size() > 0:
+        start_dialogue.emit(dialogue)
+    
+    queue_free()
