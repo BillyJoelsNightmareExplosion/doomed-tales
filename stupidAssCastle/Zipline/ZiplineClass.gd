32bd040df416d97527f7a7485725d00bd662fc4f
Alec Tremblay <tremblayalec14@gmail.com>
2023-07-17 19:35:18 -0700

    made zipline show in editor

--- stupidAssCastle/Zipline/ZiplineClass.gd
+++ stupidAssCastle/Zipline/ZiplineClass.gd
@@ -1,34 +1,29 @@
+@tool
 class_name Zipline extends Path3D
 
 @export var FOLLOW_RATE = 1
 @export var DROOP_Y = 10
-@export var INSTANCE_DISTANCE = 1.5:
-    set(value):
-        INSTANCE_DISTANCE = value
-        is_dirty = true
+@export var INSTANCE_DISTANCE = 1.5
 
-@onready var END = get_children()[0]
-
+@onready var END = $END
 @onready var player: CharacterBody3D = get_tree().root.get_children()[0].get_player()
-#@onready var END_POS = $END.position
-
 
 var path_follow
 var mm
 
 var direction = 0
 
-var is_dirty = false
 var has_player = false
 
 var init_player_pos
-var goal = 0
 var abs_progress = 0
 
 var start_col
-var end_col
 
+var last_pos
 var end_pos_local
+
+var is_initialized = false
 
 func new_collider(pos=null):
     var new_area = Area3D.new()
@@ -38,23 +33,15 @@ func new_collider(pos=null):
     new_area.add_child(new_col)
     new_col.shape = preload("res://Zipline/zipline_collision_shape.tres")
     return new_area
-     
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    end_pos_local = END.position
+
+func init():
+    is_initialized = true
+    curve.clear_points()
+    curve.add_point(Vector3.ZERO)
+    curve.add_point(Vector3.ONE)
     
-    # adding all items to tree
-    #curve = preload("res://Zipline/zipline_base_curve.tres")
-    curve.add_point(Vector3.ZERO)
-    curve.add_point(Vector3.ZERO)
-    #curve.set_point_position(0, position)
-    curve.set_point_position(1, end_pos_local)
-    var end_in = ( end_pos_local - end_pos_local/2 ) * -1
-    end_in.y = end_pos_local.y - DROOP_Y
-    curve.set_point_in(1, end_in)
     path_follow = PathFollow3D.new()
     add_child(path_follow)
-    # path_follow.loop = false
     
     var multi_mesh = MultiMeshInstance3D.new()
     add_child(multi_mesh)
@@ -70,9 +57,25 @@ func _ready():
     csg_poly.set_path_node("..")
     csg_poly.path_local = true
     csg_poly.material = preload("res://Zipline/M_zipline.tres")
+
+    update_curve()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    init()
+    
+func update_curve():    
+    if END.position != Vector3.ZERO:
+        end_pos_local = END.position
+    else:
+        end_pos_local = Vector3(1,1,1)
+    last_pos = position
+    curve.set_point_position(1, end_pos_local)
+    var end_in = ( end_pos_local - end_pos_local/2 ) * -1
+    end_in.y = end_pos_local.y - DROOP_Y
+    curve.set_point_in(1, end_in)
     
     start_col = new_collider()
-    end_col = new_collider(end_pos_local)
     
     _update_multimesh()
 
@@ -83,26 +86,28 @@ func toggle_grab_player():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    if Input.is_action_just_pressed("jump"):
-        if start_col.overlaps_body(player):
-            direction = 1
-            toggle_grab_player()
     
-    if has_player:
-        if not abs_progress:
-            path_follow.progress_ratio = 0 if direction > 0 else 1
+    if Engine.is_editor_hint():
+        if position != last_pos or end_pos_local != END.position:
+            update_curve()
+    else:
+        if Input.is_action_just_pressed("jump"):
+            if start_col.overlaps_body(player):
+                direction = 1
+                toggle_grab_player()
         
-        if abs_progress >= 1:
-            has_player = false
-            abs_progress = 0
-        else:
-            abs_progress = min(abs_progress + FOLLOW_RATE * delta, 1.0)
-            path_follow.progress_ratio = abs_progress
-            #print(path_follow.progress)
-            player.position = init_player_pos + path_follow.position
-#    if is_dirty:
-#        _update_multimesh()
-#        is_dirty = false
+        if has_player:
+            if not abs_progress:
+                path_follow.progress_ratio = 0 if direction > 0 else 1
+            
+            if abs_progress >= 1:
+                has_player = false
+                abs_progress = 0
+            else:
+                abs_progress = min(abs_progress + FOLLOW_RATE * delta, 1.0)
+                path_follow.progress_ratio = abs_progress
+                #print(path_follow.progress)
+                player.position = init_player_pos + path_follow.position
     pass
 
 func _update_multimesh():
@@ -130,6 +135,3 @@ func _update_multimesh():
         var transform = Transform3D(basis, position)
         mm.set_instance_transform(i, transform)
 
-
-func _on_curve_changed():
-    is_dirty = true
